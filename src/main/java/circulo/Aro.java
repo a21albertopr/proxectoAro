40dccd1043eaef1ade3a6d9271b5fdcb9ce28caa
Alberto Piñeiro <a21albertopr@iessanclemente.net>
2022-06-09 20:26:34 +0200

    Xeramos o Javadoc

--- src/main/java/circulo/Aro.java
+++ src/main/java/circulo/Aro.java
@@ -47,55 +47,102 @@ public class Aro {
     public void setRadio(double radio) {
         this.radio = radio;
     }
+
+    /**
+     *
+     */
     public static final double MINIMO = 0.0;
+
+    /**
+     *
+     */
     public static final double LIMITERADIO = 0.0 ;
             
     private int CoordenadaX;
     private int coordenadaY;
     private double radio;
 
+    /**
+     *
+     */
     public Aro() {
     }
 
+    /**
+     *
+     * @param valorX
+     * @param valorY
+     * @param valorRadio
+     */
     public Aro(int valorX, int valorY, double valorRadio) {
         CoordenadaX = valorX;
         coordenadaY = valorY;
         establecerRadio(valorRadio);
     }
 
+    /**
+     *
+     * @param valorX
+     */
     public void establecerX(int valorX) {
         setCoordenadaX(valorX);
     }
 
-
+    /**
+     *
+     * @param valorY
+     */
     public void establecerY(int valorY) {
         setCoordenadaY(valorY);
     }
 
-
+    /**
+     *
+     * @param valorRadio
+     */
     public void establecerRadio(double valorRadio) {
 
         setRadio(valorRadio < MINIMO ? MINIMO : valorRadio);
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double obterDiametro() {
         return getRadio() * 2;
     }
 
+    /**
+     *
+     * @return
+     */
     public double obterCircunferencia() {
         return Math.PI * obterDiametro();
     }
 
+    /**
+     *
+     * @return
+     */
     public double obterSuperficie() {
         return Math.PI * getRadio() * getRadio();
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return "Centro = [" + getCoordenadaX() + "," + getCoordenadaY() + "]; Radio = " + getRadio();
     }
 
+    /**
+     *
+     * @param trasladarX
+     * @param trasladarY
+     */
     public void trasladarCentro(int trasladarX, int trasladarY){
         setCoordenadaX(getCoordenadaX() + trasladarX);
         setCoordenadaY(getCoordenadaY() + trasladarY);
